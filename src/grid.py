bf5c72db7f478ff3b632dc25d2af1a6ad32f8a80
dgbogi564 <82741370+dgbogi564@users.noreply.github.com>
2022-11-22 15:47:58 -0500

    implemented import_grid(), generate_grid(), save_grid(), and test()

--- src/grid.py
+++ src/grid.py
@@ -6,33 +6,29 @@ import random
 import sys
 import numpy
 
-BLACK = (0, 0, 0)  # BORDERS
-GRAY = (100, 100, 100)  # BLOCKED
-GOLD = (255, 215, 0)  # HIGHWAY
-GREEN = (0, 100, 0)  # ROUGH TERRAIN
-WHITE = (255, 255, 255)  # NORMAL
 
+# TODO input grid format?
 
 class Grid:
-    normal, highway, rough_terrain, blocked = 'N', 'H', 'R', 'B'
-    up, down, left, right = 'U', 'D', 'L', 'R'
+    BLACK = (0, 0, 0)  # BORDERS
+    GRAY = (100, 100, 100)  # BLOCKED
+    GOLD = (255, 215, 0)  # HIGHWAY
+    GREEN = (0, 100, 0)  # HARD TO TRAVERSE
+    WHITE = (255, 255, 255)  # NORMAL
 
-    def __init__(self, filepath, width, height, cell_size):
-        self.window = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-        self.cell_size = cell_size
-        self.circle_radius = 10
-        self.border_size = 2
-        self.line_width = 5
-        self.grid = self.import_grid(filepath)
-
-    def draw_grid(self):
-
-
+    NORMAL, HIGHWAY, HARD_TO_TRAVERSE, BLOCKED = 'N', 'H', 'T', 'B'
+    UP, DOWN, LEFT, RIGHT = 'U', 'D', 'L', 'R'
 
     @classmethod
     def import_grid(cls, filepath):
-        # TODO
         grid = None
+        with open(filepath, "r") as file:
+            rows, columns = map(int, file.readline().split(' '))
+            grid = [[''] * columns] * rows
+            for line in file:
+                r, c, t = line.split()
+                row, column, terrain = int(r), int(c), t
+                grid[row][column] = terrain
         return grid
 
     @classmethod
@@ -42,59 +38,82 @@ class Grid:
             for column in range(columns):
                 num = random.uniform(0, 1)
                 if 0 <= num <= 0.5:
-                    grid[row][column] = Grid.normal
+                    grid[row][column] = Grid.NORMAL
                 elif 0.5 < num <= 0.7:
-                    grid[row][column] = Grid.highway
+                    grid[row][column] = Grid.HIGHWAY
                 elif 0.7 < num <= 0.9:
-                    grid[row][column] = Grid.rough_terrain
+                    grid[row][column] = Grid.HARD_TO_TRAVERSE
                 else:
-                    grid[row][column] = Grid.blocked
+                    grid[row][column] = Grid.BLOCKED
         return grid
 
     @classmethod
-    def generate_ground_truth_paths(cls, grid):
-        # TODO we don't know much about map format besides that it is 100 by 50?
+    def save_grid(cls, grid):
+        rows, columns = len(grid), len(grid[0])
+        filepath = f'grid_{datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")}.txt'
+        with open(filepath, "w") as file:
+            file.write(f'{rows} {columns}\n')
+            for row in range(rows):
+                for column in range(columns):
+                    file.write(f'{row} {column} {grid[row][column]}\n')
+        return filepath
+
+    @classmethod
+    def draw_grid(cls, grid):
+        # TODO
+        width, height = 1000, 1000
+        window = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+
+        cell_size = 150
+        circle_radius = 10
+        border_size = 2
+        font_size = 35
+        line_width = 5
+
+    @classmethod
+    def generate_ground_truth_paths(cls, grid, probabilities):
         row, column = -1, -1
+        starting_row, starting_column = -1, -1
         rows, columns = len(grid), len(grid[0])
         coordinates = [(r, c) for r in range(rows) for c in range(columns)]
 
         # check if unblocked cells exist
-        if not any(terrain in row for terrain in [Grid.normal, Grid.highway, Grid.rough_terrain] for row in grid):
+        if not any(terrain in row for terrain in [Grid.NORMAL, Grid.HIGHWAY, Grid.HARD_TO_TRAVERSE] for row in grid):
             raise Exception("No unblocked cells exist")
 
         # get random unblocked cell
         while row == -1:
             r, c = random.choice(coordinates)
-            if grid[r][c] != Grid.blocked:
+            if grid[r][c] != Grid.BLOCKED:
                 starting_row = row = r
                 starting_column = column = c
 
         # generate actions, ground truth states, and sensor readings
-        actions = ''
-        ground_truth_states = ''
-        sensor_readings = ''
+        actions = []  # actions = ''
+        ground_truth_states = []  # ground_truth_states = ''
+        sensor_readings = []  # sensor_readings = ''
         for x in range(100):
             # actions
-            action = random.choice([Grid.up, Grid.down, Grid.left, Grid.right])
-            actions += action
+            action = random.choice([Grid.UP, Grid.DOWN, Grid.LEFT, Grid.RIGHT])
+            actions.append(action)  # actions += action
 
             # ground truth states
             num = random.uniform(0, 1)
             if 0 <= num < 0.9:
                 match action:
-                    case Grid.up:
-                        if row > 0 and grid[row - 1][column] != Grid.blocked:
+                    case Grid.UP:
+                        if row > 0 and grid[row - 1][column] != Grid.BLOCKED:
                             row -= 1
-                    case Grid.down:
-                        if row < rows - 1 and grid[row + 1][column] != Grid.blocked:
+                    case Grid.DOWN:
+                        if row < rows - 1 and grid[row + 1][column] != Grid.BLOCKED:
                             row += 1
-                    case Grid.left:
-                        if column > 0 and grid[row][column - 1] != Grid.blocked:
+                    case Grid.LEFT:
+                        if column > 0 and grid[row][column - 1] != Grid.BLOCKED:
                             column -= 1
-                    case Grid.right:
-                        if column < columns - 1 and grid[row][column + 1] != Grid.blocked:
+                    case Grid.RIGHT:
+                        if column < columns - 1 and grid[row][column + 1] != Grid.BLOCKED:
                             column += 1
-            ground_truth_states += f'{row} {column}\n'
+            ground_truth_states.append(f'{row} {column}')  # ground_truth_states += f'{row} {column}\n'
 
             # sensor readings
             num = random.uniform(0, 1)
@@ -102,26 +121,68 @@ class Grid:
                 sensor_reading = grid[row][column]
             else:
                 sensor_reading = random.choice(
-                    [x for x in [Grid.normal, Grid.highway, Grid.rough_terrain] if x != grid[row][column]])
-            sensor_readings += sensor_reading
+                    [x for x in [Grid.NORMAL, Grid.HIGHWAY, Grid.HARD_TO_TRAVERSE] if x != grid[row][column]])
+            sensor_readings.append(sensor_reading)  # sensor_readings += sensor_reading
 
         # write everything to a file
-        with open(f'ground_truth_path_{datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")}.txt', "w") as file:
+        filepath = f'ground_truth_path_{datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")}.txt'
+        with open(filepath, "w") as file:
             # write coordinates of initial cell
             file.write(f'{starting_row} {starting_column}\n')
             # write ground truth states
-            file.write(ground_truth_states)
+            file.write('\n'.join(ground_truth_states) + '\n')
             # write actions
-            file.write(actions + '\n')
+            file.write(''.join(actions) + '\n')
             # write sensor readings
-            file.write(sensor_readings + '\n')
+            file.write(''.join(sensor_readings) + '\n')
+
+        return filepath
+
+    @classmethod
+    def calculate_probabilities(cls, grid, ground_truth_paths):
+        # TODO
+
 
 
 def test():
-    Grid.generate_ground_truth_paths(Grid.generate_grid(100, 50))
+    grid = None
+    try:
+        grid = Grid.generate_grid(3, 4)
+        print('[PASSED] Generate grid\n')
+    except Exception as e:
+        print('[FAILED] Generate grid:\n' + e.__str__() + '\n\n')
+        exit(1)
 
-def bad_test():
-    Grid.generate_ground_truth_paths([[Grid.blocked] * 100] * 50)
+    grid_filepath = None
+    if grid is not None:
+        try:
+            grid_filepath = Grid.save_grid(grid)
+            print('[PASSED] Save grid\n')
+        except Exception as e:
+            print('[FAILED] Save grid:\n' + e.__str__() + '\n\n')
+
+        try:
+            Grid.generate_ground_truth_paths(grid)
+            print("[PASSED] Generate ground truth paths of randomly generated grid\n")
+        except Exception as e:
+            print("[FAILED] Generate ground truth paths of randomly generated grid:\n" + e.__str__() + '\n\n')
+
+    try:
+        Grid.generate_ground_truth_paths([[Grid.BLOCKED] * 100] * 50)
+        print("[FAILED] Generate ground truth paths of blocked grid\n")
+    except Exception as e:
+        if e.__str__() == "No unblocked cells exist":
+            print("[PASSED] Generate ground truth paths of blocked grid\n")
+
+    if grid_filepath is not None:
+        try:
+            imported_grid = Grid.import_grid(grid_filepath)
+            if imported_grid == grid:
+                print('[PASSED] Import grid\n')
+            else:
+                raise Exception('Grid mismatch')
+        except Exception as e:
+            print('[FAILED] Import grid:\n' + e.__str__() + '\n\n')
 
 
 if __name__ == '__main__':
